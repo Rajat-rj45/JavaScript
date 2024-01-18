b0929a36a4a23208deacd884ccd4ec8df8514f35
Rajat Kumar <68491674+Rajat-rj45@users.noreply.github.com>
2024-01-18 18:41:43 +0000

    let/const/var

---
+++ Basic/variable.js
@@ -0,0 +1,27 @@
+
+
+console.log(account1);
+const accountId = 144532;
+
+let accountEmail = "rajat@gmail.com";
+var accountPassword ="12345";
+accountCity = "jaipur";
+let account;
+var account1;
+
+//accountId = 2;
+
+
+data();
+
+
+//console.table([accountId, accountEmail, accountPassword, accountCity, account1,account])
+
+function data() {
+    var a=2;
+    let b=3;
+    const c=4;
+    console.table([accountId, accountEmail, accountPassword, accountCity, account])   
+}
+
+console.log(a);
